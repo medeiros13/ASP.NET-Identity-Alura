cc56b6f9ee2dd35f0fbd8109e22715749dfa1401
medeiros13 <gabrielmedeiros13122000@gmail.com>
2021-06-16 14:41:52 -0300

    Owin configurado

--- ByteBank.Forum/Controllers/ContaController.cs
+++ ByteBank.Forum/Controllers/ContaController.cs
@@ -2,6 +2,7 @@
 using ByteBank.Forum.ViewModels;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,6 +14,25 @@ namespace ByteBank.Forum.Controllers
 {
     public class ContaController : Controller
     {
+        private UserManager<UsuarioAplicacao> _userManager;
+        public UserManager<UsuarioAplicacao> UserManager
+        {
+            get
+            {
+                if (_userManager == null)
+                {
+                    var contextOwin = HttpContext.GetOwinContext();
+                    _userManager = contextOwin.GetUserManager<UserManager<UsuarioAplicacao>>();
+                }
+
+                return _userManager;
+            }
+            set
+            {
+                _userManager = value;
+            }
+        }
+
         public ActionResult Registrar()
         {
             return View();
@@ -23,13 +43,6 @@ namespace ByteBank.Forum.Controllers
         {
             if (ModelState.IsValid)
             {
-                var dbContext = new IdentityDbContext<UsuarioAplicacao>("DefaultConnection");
-                //A classe que conversa com o dbContext (banco de dados) é a UserStore
-                //Possui uma propriedade boolean chamada AutoSaveChanges, quando está está marcada como True, sempre que dermos um userStore.Create/userStore.Update/userStore.Delete, automaticamente vai salvar no dbContext (banco de dados)
-                //O usarmos as classes IdentityDbContext, UserStore e a UserManager, nossa aplicação independe do tipo de banco de dados que está sendo utilizado,
-                //pois o banco será definido na ConnectionString passada por parâmetro no construtor do IdentityDbContext, podendo ser SQL Server, MySQl, PL\SQL entre outros
-                var userStore = new UserStore<UsuarioAplicacao>(dbContext);
-                var userManager = new UserManager<UsuarioAplicacao>(userStore);
 
                 var novoUsuario = new UsuarioAplicacao();
 
@@ -39,7 +52,7 @@ namespace ByteBank.Forum.Controllers
 
                 //Alterado o Create por CreateAsync, para que a operação seja assíncrona, foi necessário também adicionar o await na chamada do método,
                 //e na declaração dessa função aqui, adicionamos o async e mudamos o tipo de ActionResult para Task<ActionResult>
-                await userManager.CreateAsync(novoUsuario, modelo.Senha);
+                await UserManager.CreateAsync(novoUsuario, modelo.Senha);
                 //Podemos incluir o usuário
                 return RedirectToAction("Index", "Home");
             }
---
+++ ByteBank.Forum/Startup.cs
@@ -0,0 +1,43 @@
+﻿using ByteBank.Forum.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
+using Microsoft.AspNet.Identity.Owin;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+//Aqui definimos que o tipo que será usado na classe de inicialização do Owin (OwinStartup), será a nossa classe (ByteBank.Forum.Startup)
+[assembly: OwinStartup(typeof(ByteBank.Forum.Startup))]
+namespace ByteBank.Forum
+{
+    public class Startup
+    {
+        //Aqui configuramos todo o contexto do Owin, esse contexto será montado sempre que uma requisição é feita à nossa aplicação (GET,PUT,PATCH,DELETE,POST,etc...)
+        public void Configuration(IAppBuilder builder)
+        {
+            //Aqui definimos o dbContext que o Owin deve usar
+            builder.CreatePerOwinContext<DbContext>(() =>
+                new IdentityDbContext<UsuarioAplicacao>("DefaultConnection"));
+
+            //Aqui definimos o IUserStore que o Owin deve usar (usamos a interface ao invés da classe para não termos que depender do EntityFramework)
+            //Foi utilizada a expressão lambda abaixo para resgatar o mesmo dbContext setado na chamada acima
+            builder.CreatePerOwinContext<IUserStore<UsuarioAplicacao>>(
+                (opcoes, contextoOwin) =>
+                {
+                    var dbContext = contextoOwin.Get<DbContext>();
+                    return new UserStore<UsuarioAplicacao>(dbContext);
+                });
+
+            builder.CreatePerOwinContext<UserManager<UsuarioAplicacao>>(
+                (opcoes, contextoOwin) =>
+                {
+                    var userStore = contextoOwin.Get<IUserStore<UsuarioAplicacao>>();
+                    return new UserManager<UsuarioAplicacao>(userStore);
+                });
+        }
+    }
+}
