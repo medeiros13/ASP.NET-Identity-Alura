058225a76d1ae2f19aabb4082382fea7fd42dab2
medeiros13 <gabrielmedeiros13122000@gmail.com>
2021-06-16 11:59:48 -0300

    Adicionados comentários e print da arquitetura da aplicação

--- ByteBank.Forum/Controllers/ContaController.cs
+++ ByteBank.Forum/Controllers/ContaController.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNet.Identity.EntityFramework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -18,11 +19,15 @@ namespace ByteBank.Forum.Controllers
         }
 
         [HttpPost]
-        public ActionResult Registrar(ContaRegistrarViewModel modelo)
+        public async Task<ActionResult> Registrar(ContaRegistrarViewModel modelo)
         {
             if (ModelState.IsValid)
             {
                 var dbContext = new IdentityDbContext<UsuarioAplicacao>("DefaultConnection");
+                //A classe que conversa com o dbContext (banco de dados) é a UserStore
+                //Possui uma propriedade boolean chamada AutoSaveChanges, quando está está marcada como True, sempre que dermos um userStore.Create/userStore.Update/userStore.Delete, automaticamente vai salvar no dbContext (banco de dados)
+                //O usarmos as classes IdentityDbContext, UserStore e a UserManager, nossa aplicação independe do tipo de banco de dados que está sendo utilizado,
+                //pois o banco será definido na ConnectionString passada por parâmetro no construtor do IdentityDbContext, podendo ser SQL Server, MySQl, PL\SQL entre outros
                 var userStore = new UserStore<UsuarioAplicacao>(dbContext);
                 var userManager = new UserManager<UsuarioAplicacao>(userStore);
 
@@ -32,7 +37,9 @@ namespace ByteBank.Forum.Controllers
                 novoUsuario.UserName = modelo.UserName;
                 novoUsuario.NomeCompleto = modelo.NomeCompleto;
 
-                userManager.Create(novoUsuario, modelo.Senha);
+                //Alterado o Create por CreateAsync, para que a operação seja assíncrona, foi necessário também adicionar o await na chamada do método,
+                //e na declaração dessa função aqui, adicionamos o async e mudamos o tipo de ActionResult para Task<ActionResult>
+                await userManager.CreateAsync(novoUsuario, modelo.Senha);
                 //Podemos incluir o usuário
                 return RedirectToAction("Index", "Home");
             }
